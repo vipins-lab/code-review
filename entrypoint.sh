dfe98a0d03e8755fbc7615ef41089f3052eea9ec
vipinkrajput <vipin.hbti.in@gmail.com>
2024-02-19 16:28:58 -0800

    silent pip output

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,8 +15,8 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip3 install --upgrade pip
 pip install pip-tools
-pip-compile
-pip-sync
+pip-compile -q
+pip-sync -q
 python3 reviewer-auto-assignment.py "$1" "$2"
 
 echo "message=Reviewers assinged to the Pull Requests." >> $GITHUB_OUTPUT
